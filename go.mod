98aa57fb5b97e90bce6192e8f29bb97299cf4ca8
John Sushant Sundharam <jsundhar@jsundhar-mn1.linkedin.biz>
2020-02-09 18:53:09 +0530

    Base functionality developed

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/johnsushant/pod-crash-controller
+
+go 1.13
+
+require (
+	github.com/coreos/etcd v3.3.15+incompatible
+	github.com/go-logr/logr v0.1.0
+	github.com/go-logr/zapr v0.1.0
+	go.uber.org/zap v1.9.1
+	k8s.io/api v0.0.0-20190918155943-95b840bb6a1f
+	k8s.io/apimachinery v0.0.0-20190913080033-27d36303b655
+	sigs.k8s.io/controller-runtime v0.4.0
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/coreos/etcd/client"
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var restartList map[string]int32
+
+func slackNotification(message string) {
+	println(message)
+}
+
+type ReconcilePod struct {
+	Client client.Client
+	Logger logr.Logger
+}
+
+func (r *ReconcilePod) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	log := r.Logger
+	pod := &corev1.Pod{}
+	err := r.Client.Get(context.Background(), request.NamespacedName, pod)
+	if errors.IsNotFound(err) {
+		log.Error(nil, "Pod Not Found. Could have been deleted")
+		return reconcile.Result{}, nil
+	}
+	if err != nil {
+		log.Error(err, "Error fetching pod. Going to requeue")
+		return reconcile.Result{Requeue: true}, err
+	}
+	for i := range pod.Status.ContainerStatuses {
+		container := pod.Status.ContainerStatuses[i].Name
+		restartCount := pod.Status.ContainerStatuses[i].RestartCount
+		identifier := pod.Name + pod.Status.ContainerStatuses[i].Name
+		if _, ok := restartList[identifier]; !ok {
+			restartList[identifier] = restartCount
+		} else if restartList[identifier] < restartCount {
+			slackNotification(fmt.Sprintf("%s container of %s pod restarted. Total restart count: %d", container, pod.Name, restartCount))
+			restartList[identifier] = restartCount
+		}
+	}
+	return reconcile.Result{}, nil
+}
+
+func main() {
+	log := zapr.NewLogger(zap.NewExample()).WithName("pod-crash-controller")
+	restartList = make(map[string]int32)
+
+	log.Info("Setting up manager")
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	if err != nil {
+		log.Error(err, "Unable to setup manager. Please check if KUBECONFIG is available")
+		os.Exit(1)
+	}
+
+	log.Info("Setting up controller")
+	ctrl, err := controller.New("pod-crash-controller", mgr, controller.Options{
+		Reconciler: &ReconcilePod{Client: mgr.GetClient(), Logger: log},
+	})
+	if err != nil {
+		log.Error(err, "Failed to setup controller")
+		os.Exit(1)
+	}
+
+	log.Info("Watching Pods")
+	if err := ctrl.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		log.Error(err, "Failed to watch pods")
+		os.Exit(1)
+	}
+
+	log.Info("Starting the manager")
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Error(err, "Failed to start manager")
+		os.Exit(1)
+	}
+}
